93f8ae814cc9503aad4922cd0360191ba2ca550a
xiangbei <xiangbei86@163.com>
2021-12-12 21:08:32 +0800

    首次共享项目

---
+++ src/main/java/org/example/ttt.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @Author : zhuang
+ * @GenDate : 2021-12-12 21:06
+ * @Description : TODO
+ */
+public class ttt {
+    public static void main(String[] args) {
+        System.out.println("hello idea github");
+    }
+}
